693992226844d7989672c2bcd2725280954d89f4
Paul Thomas <paulthomas2107@gmail.com>
2023-01-20 11:13:51 +0000

    More

---
+++ src/main/java/com/pault/course/Main.java
@@ -0,0 +1,14 @@
+package com.pault.course;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame();
+
+        frame.setVisible(true);
+
+    }
+}
